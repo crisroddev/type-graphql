da9488c28a035702c53a3abf8a92bafb753c37f4
CrisRod <crodriguez85@gmail.com>
2019-06-17 13:23:43 -0400

    Register Resolver Finished

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,27 @@
 import "reflect-metadata";
-import { ApolloServer } from 'apollo-server-express';
-import express from 'express';
-import { buildSchema } from 'type-graphql';
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import { buildSchema } from "type-graphql";
 import { createConnection } from "typeorm";
 
-import { RegisterResover } from "./modules/user/Register";
+import { RegisterResolver } from "./modules/user/Register";
 
 const main = async () => {
-    await createConnection();
-  
-    const schema = await buildSchema({
-      resolvers: [RegisterResover]
-    });
-  
-    const apolloServer = new ApolloServer({ schema });
-  
-    const app = express();
-  
-    apolloServer.applyMiddleware({ app });
-  
-    app.listen(4000, () => {
-      console.log("server started on http://localhost:4000/graphql");
-    });
-  };
-  
+  await createConnection();
+
+  const schema = await buildSchema({
+    resolvers: [RegisterResolver]
+  });
+
+  const apolloServer = new ApolloServer({ schema });
+
+  const app = express();
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started on http://localhost:4000/graphql");
+  });
+};
 
 main();
