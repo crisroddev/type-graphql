8edd4d5a57a96266d3da89e11c58f45cc2af796d
CrisRod <crodriguez85@gmail.com>
2019-06-19 11:50:41 -0400

    Nodemailer basic

---
+++ src/modules/utils/sendEmail.ts
@@ -0,0 +1,30 @@
+import nodemailer from 'nodemailer';
+
+export async function sendEmail(){
+  let testAccount = await nodemailer.createTestAccount();
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass // generated ethereal password
+    }
+  });
+
+  const mailOptions = await transporter.sendMail({
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: "bar@example.com, baz@example.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>" // html body
+  });
+
+  const info = await transporter.sendEmail(mailOptions);
+
+  console.log("Message sent: %s", info.messageId);
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+}
+
