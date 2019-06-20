35bdd87dff3a4b6a79c59f5987896f7a7dc62e71
CrisRod <crodriguez85@gmail.com>
2019-06-20 11:18:29 -0400

    Change Passwords with extends Password input

--- src/modules/user/changePassword/ChangePasswordInput.ts
+++ src/modules/user/changePassword/ChangePasswordInput.ts
@@ -1,10 +1,8 @@
+import { PasswordInput } from './../../shared/PasswordInput';
 import { InputType, Field } from "type-graphql";
 
 @InputType()
-export class ChangePasswordInput {
+export class ChangePasswordInput extends PasswordInput{
     @Field()
     token: string;
-
-    @Field()
-    password: string;
 }
