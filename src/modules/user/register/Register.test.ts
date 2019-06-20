378c596b9ddcac93ecc741da493a0c73b4b0a58f
CrisRod <crodriguez85@gmail.com>
2019-06-20 14:41:20 -0400

    Starting Register User Test

--- src/modules/user/register/Register.test.ts
+++ src/modules/user/register/Register.test.ts
@@ -1,5 +1,20 @@
 import { testConn } from "src/test-utils/testConn";
+import { Connection } from "typeorm";
+import { graphql } from "graphql";
+
+let conn: Connection
 
 beforeAll( async () => {
     await testConn();
+})
+afterAll( async () => {
+    await conn.close()
+})
+
+describe('Register', () => {
+    it("create user", () => {
+       graphql({
+           
+       })
+    })
 })
